94043cfed674bcd460f90ffa36b0f9c6bf952bdc
Anupam Dahal <miracleanupam@gmail.com>
2021-11-16 14:08:55 +0100

    DTU countdown

--- countdown/index.js
+++ countdown/index.js
@@ -38,7 +38,7 @@ function initializeClock(id, endtime) {
       c.style.display = "none";
       var text = document.getElementById('message');
       var title = document.getElementById('title');
-      text.innerHTML = "Finally! I'm free mofos!"
+      text.innerHTML = "Finally! Good Riddance DTU!"
       title.style.display = "none";
     }
     if (t.days <= 0) {
@@ -64,5 +64,5 @@ function initializeClock(id, endtime) {
   var timeinterval = setInterval(updateClock, 1000);
 }
 
-var deadline = new Date("Aug 31, 2021 18:00:00");
+var deadline = new Date("Dec 17, 2021 18:00:00");
 initializeClock('clockdiv', deadline);
